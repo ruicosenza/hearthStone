1b005014c25ed7bc616f98f414a36aef3bec93e6
Rui Gomes Cosenza <ruigomes@tjrj.jus.br>
2020-07-03 20:18:24 -0300

    primeiro commit: pom.xml com as dependências

---
+++ src/main/java/com/br/temasistemas/hearthstone/HearthstoneApplication.java
@@ -0,0 +1,13 @@
+package com.br.temasistemas.hearthstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HearthstoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HearthstoneApplication.class, args);
+	}
+
+}
